7afebe046acdd464efd2a7e835a94c0842c18d73
Rodrigo Cordeiro <rodrigomendoncca@gmail.com>
2020-01-02 08:12:55 -0300

    Update helloworld/urls.py

--- website/urls.py
+++ website/urls.py
@@ -1,3 +1,10 @@
+# Importamos a função index() definida no arquivo views.py
 from . import views
 
 app_name = 'website'
+
+# urlpatterns a lista de roteamentos de URLs para funções/Views
+urlpatterns = [
+    # GET /
+    path('funcionarios/<int:ano>/', views.funcionarios_por_ano),
+]
